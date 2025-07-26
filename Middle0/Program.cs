6622216c88a27713c896a2ef1b68c18d970f6e5e
Lisin Ivan <1985interdom@gmail.com>
2025-07-26 21:07:52 +0300

    MongoDB error1

--- Middle0/Program.cs
+++ Middle0/Program.cs
@@ -5,6 +5,7 @@ using Middle0.Persistence.Context;
 using Middle0.Persistence.Repositories;
 using Middle0.Persistence.Repositories.Interfaces;
 using Middle0.Persistence.SeedData;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -35,11 +36,32 @@ builder.Services.AddCors(options =>
 });
 
 builder.Services.AddDbContext<EventDbContext>(options =>
-	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionDocker")));
 
 var app = builder.Build();
 
 app.UseCors(MyAllowSpecificOrigins);
+
+//---------------------------------------------------------------------------------
+
+Log.Logger = new LoggerConfiguration()
+	.WriteTo.MongoDB(
+		databaseUrl: "mongodb://localhost:8080/logs",   // строка подключения и БД
+		collectionName: "logevents"                      // имя коллекции
+	)
+	.Enrich.FromLogContext()
+	.Enrich.WithProperty("Application", "MyApp")          // необязательное добавление контекста
+	.CreateLogger();
+
+builder.Host.UseSerilog();
+
+app.MapGet("/", () =>
+{
+	Log.Information("Это информационное сообщение");      // пример лога
+	return "Hello World!";
+});
+
+//--------------------------------------------------------------------------------
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
