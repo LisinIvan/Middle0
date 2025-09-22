827d3dc62b1c173f07ecd965ef9598eb784591d2
Lisin Ivan <1985interdom@gmail.com>
2025-09-22 10:02:10 +0300

    Registration 3

--- Middle0.Domain/Common/DTO/RegisterDto.cs
+++ Middle0.Domain/Common/DTO/RegisterDto.cs
@@ -1,11 +1,21 @@
 ﻿
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Middle0.Domain.Common.DTO
 {
     public class RegisterDto
     {
+		[Required(ErrorMessage = "Имя пользователя обязательно")]
+		[StringLength(20, MinimumLength = 3, ErrorMessage = "Имя пользователя должно быть от 3 до 20 символов")]
 		public string Username { get; set; }
+
+		[Required(ErrorMessage = "Email обязателен")]
+		[EmailAddress(ErrorMessage = "Введите корректный email")]
 		public string Email { get; set; }
+
+		[Required(ErrorMessage = "Пароль обязателен")]
+		[StringLength(100, MinimumLength = 6, ErrorMessage = "Пароль должен содержать минимум 6 символов")]
 		public string Password { get; set; }
 	}
 }
